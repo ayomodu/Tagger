996843f51e20bc85e1448a3b91e24e0408f84107
ayomodu <ayomodu@gmail.com>
2023-04-09 20:23:31 -0400

    finished writing plugin script and api

---
+++ Plugin/plugin.sh
@@ -0,0 +1,63 @@
+set -eou pipefail
+send_post_request(){
+    case $DRONE_DEPLOY_TO in
+    "")
+        payload="{\"project_name\":\"$DRONE_REPO_NAME\", \"development_version\":\"$version\"}"
+        curl --request POST \
+        --url http://localhost:8000/api/add_project/ \
+        --header 'accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --data "$payload"
+        ;;
+    staging)
+        payload="{\"project_name\":\"$DRONE_REPO_NAME\", \"staging_version\":\"$version\"}"
+        curl --request POST \
+        --url http://localhost:8000/api/add_project/ \
+        --header 'accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --data "$payload"
+        ;;
+    production)
+        payload="{\"project_name\":\"$DRONE_REPO_NAME\", \"production_version\":\"$version\"}"
+        curl --request POST \
+        --url http://localhost:8000/api/add_project/ \
+        --header 'accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --data "$payload"
+        ;;
+    esac
+}
+
+
+get_semantic_version() {
+    # Check if a file exists and read its contents
+    if [[ -e "./$PLUGIN_PROJECT_NAME/$PLUGIN_PROJECT_NAME'.csproj" ]]; then
+        version=$(awk -F "[><]" '/<Version>/{a=$3}END{print a}' ./$PLUGIN_PROJECT_NAME/$PLUGIN_PROJECT_NAME.csproj)
+        echo "deploying for development"
+        send_post_request
+    elif [[ -e "package.json" ]]; then
+        version=$(grep -oP '"version": "\K.*?(?=")' package.json)
+        echo "deploying for staging"
+        send_post_request
+    elif [[ -e "pom.xml" ]]; then
+        version=$(awk -F "[><]" '/<version>/{print $3;exit}' pom.xml)
+        echo "deploying for production"
+        send_post_request
+    else
+        echo "Error: could not find project configuration file"
+        return 1
+    fi
+
+    # Check if the version matches the semantic versioning format
+    if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$ ]]; then
+        echo "$version"
+    else
+        echo "Error: version is not in semantic versioning format"
+        return 1
+    fi
+}
+
+DRONE_REPO_NAME="alternatereponame"
+DRONE_DEPLOY_TO="staging"
+version="10.9.4"
+
--- api/api.py
+++ api/api.py
@@ -37,16 +37,16 @@ def add_project(item: Item):
     except:
         print(f"Project {project_name} already exists")
         if development_version != None:
-            cursor.execute(f'insert into tagger (project_name,development) values("{project_name}", "{development_version}") \
-                           on conflict(project_name) do update set development=excluded.development')
+            cursor.execute('insert into tagger (project_name,development) values(?, ?) \
+                           on conflict(project_name) do update set development=excluded.development', (project_name, development_version))
             return {"Success": f"Project {project_name} development version updated to: {development_version}"}
         if staging_version != None:
-            cursor.execute(f'insert into tagger (project_name,staging) values("{project_name}", "{staging_version}") \
-                           on conflict(project_name) do update set staging=excluded.staging')
+            cursor.execute('insert into tagger (project_name,staging) values(?, ?) \
+                           on conflict(project_name) do update set staging=excluded.staging', (project_name, staging_version))
             return {"Success": f"Project {project_name} staging version updated to: {staging_version}"}
         if production_version != None:
-            cursor.execute(f'insert into tagger (project_name,production) values("{project_name}", "{production_version}") \
-                           on conflict(project_name) do update set production=excluded.production')
+            cursor.execute('insert into tagger (project_name,production) values(?, ?) \
+                           on conflict(project_name) do update set production=excluded.production', (project_name, production_version))
             return {"Success": f"Project {project_name} production version updated to: {production_version}"}
     finally:
         connection.commit()
@@ -54,14 +54,39 @@ def add_project(item: Item):
     
     
 
-# @app.get("/api/get_projects/")
-# def get_all():
-#     try:
-#         connection = sqlite3.connect('tagger.db')
-#         cursor = connection.cursor()
-#         cursor.execute("SELECT * FROM tagger")
-#         results = cursor.fetchall()
-#         return results
+@app.get("/api/get_all_projects")
+def get_all():
+    try:
+        connection = sqlite3.connect('tagger.db')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM tagger")
+        results = cursor.fetchall()
+        for i in results:
+            entry = [ {"Project_name": i[0], "development" : i[1], "staging": i[2], "production": i[3]} for i in results ]
+        return entry
+    except:
+        return {"Error" : "Could not fetch projects"}
+    finally:
+        connection.commit()
+        connection.close()
+
+@app.get("/api/get_projects")
+def get_all(name: str):
+    proj_name = name
+    try:
+        connection = sqlite3.connect('tagger.db')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM tagger WHERE project_name LIKE ?", ('%'+proj_name+'%',))
+        results = cursor.fetchall()
+        for i in results:
+            entry = [ {"Project_name": i[0], "development" : i[1], "staging": i[2], "production": i[3]} for i in results ]
+        return entry
+    except:
+        return {"Error" : "Could not fetch projects"}
+    finally:
+        connection.commit()
+        connection.close()
+    
 
 
     
